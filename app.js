3b51554f5eedaf15a0299b1830bd3e6a590de256
gaozhong <53365381@qq.com>
2020-12-31 09:43:32 +0800

    dist

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 //app.js
 App({
 
-  // 全局变量
+  // 全局变量 
   globalData: {},
 
   // 预加载
-  onLaunch: function () {},
+  onLaunch: function () { },
 
   // 进入小程序 / 后台切换
-  onShow() {},
+  onShow() { },
 
   // 进入后台
-  onHide() {},
+  onHide() { },
 
 })
